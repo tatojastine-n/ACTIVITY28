bf202d3844d98d44c0658ad5d32ec9549ac32941
tatojastine-n <jajanicoletato@gmail.com>
2025-08-08 22:03:53 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManualWordReversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(word))
+            {
+                Console.WriteLine("Error: No word entered.");
+                return;
+            }
+            Console.Write("Reversed word: ");
+
+            //start from the last character and move backwards
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                Console.Write(word[i]);
+            }
+            Console.WriteLine(); //for clean output
+        }
+    }
+}
